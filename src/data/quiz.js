18da72d978e8fc45a7625b01ba33ebe73faf1637
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-11-12 13:21:39 +0400

    rename

---
+++ src/data/quiz.js
@@ -0,0 +1,437 @@
+[
+  {
+    questionText: "What is WordPress?",
+    options: [
+      {
+        option: "WordPress is a server",
+        isCorrect: false,
+      },
+      {
+        option: "WordPress is a programming language",
+        isCorrect: false,
+      },
+      {
+        option: "JavaScript is a Zoo",
+        isCorrect: false,
+      },
+      {
+        option: "WordPress is a Content Management System",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questionText:
+      "How many user roles available in wordpress first installation?",
+    options: [
+      {
+        option: "4",
+        isCorrect: false,
+      },
+      {
+        option: "50",
+        isCorrect: false,
+      },
+      {
+        option: "5",
+        isCorrect: true,
+      },
+      {
+        option: ".5",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is XAMPP?",
+    options: [
+      {
+        option: "XAMPP is a operating system",
+        isCorrect: false,
+      },
+      {
+        option: "XAMPP is a fruits",
+        isCorrect: false,
+      },
+      {
+        option: "I don't even know!",
+        isCorrect: false,
+      },
+      {
+        option:
+          "XAMPP is a free and open-source cross-platform web server solution",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questionText: "What does it mean by a theme in wordpress?",
+    options: [
+      {
+        option: "Themes are used for making orange juice.",
+        isCorrect: false,
+      },
+      {
+        option: "Themes are used for building bridge.",
+        isCorrect: false,
+      },
+      {
+        option: "Themes defines website structures and designs.",
+        isCorrect: true,
+      },
+      {
+        option: "All of the above.",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is plugins?",
+    options: [
+      {
+        option: "Plugins is a super computer.",
+        isCorrect: false,
+      },
+      {
+        option:
+          "plugin is a software add-on that is installed on a program, enhancing its functionalities.",
+        isCorrect: true,
+      },
+      {
+        option: "A car name",
+        isCorrect: false,
+      },
+      {
+        option: "Database",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "Author can _____________?",
+    options: [
+      {
+        option: "Edit it's won post.",
+        isCorrect: true,
+      },
+      {
+        option: "Delete post",
+        isCorrect: false,
+      },
+      {
+        option: "Upload image",
+        isCorrect: false,
+      },
+      {
+        option: "Nothing",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "Editor can ____________?",
+    options: [
+      {
+        option: "Uninstall wordpress",
+        isCorrect: false,
+      },
+      {
+        option: "Update themes and plugins",
+        isCorrect: false,
+      },
+      {
+        option: "Can play football with cricket ball.",
+        isCorrect: false,
+      },
+      {
+        option: "Make a page, Create, Read, Update, Delete Post",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    questionText: "What is permalink?",
+    options: [
+      {
+        option: "I saw it for the first time since my birth.",
+        isCorrect: false,
+      },
+      {
+        option: "Page or post link which is displayed in browser address bar.",
+        isCorrect: true,
+      },
+      {
+        option: "A snake name in ludo-club",
+        isCorrect: false,
+      },
+      {
+        option: "All of the above.",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "Whose are the post category??",
+    options: [
+      {
+        option: "Post Title, Appearance, Dashboard, Menus",
+        isCorrect: false,
+      },
+      {
+        option: "Lorem ipsum dolor sit amet.",
+        isCorrect: false,
+      },
+      {
+        option: "WordPress, Web Development, Programming, Sports, Hobby",
+        isCorrect: true,
+      },
+      {
+        option: "All of the above",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "WordPress can not make all kind of websites?",
+    options: [
+      {
+        option: "HA HA HA HA HA HA HA HA HA HA HA",
+        isCorrect: true,
+      },
+      {
+        option: "Stop, stop, I can't lough anymore!!",
+        isCorrect: true,
+      },
+      {
+        option: "পাড়ায় আসিস, বার্নিশ করে ছেড়ে দেব।",
+        isCorrect: true,
+      },
+      {
+        option: "May be!!!!!!!!!!!!",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText: "What is theme option?",
+    options: [
+      {
+        option: "All of the settings where we can modify our entire site.",
+        isCorrect: false,
+      },
+      {
+        option: "It's a dumb train",
+        isCorrect: false,
+      },
+      {
+        option: "A option where we can customize widget panel",
+        isCorrect: false,
+      },
+      {
+        option: "Option 1 & 3 both",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText: "What is blog post featured image?",
+    options: [
+      {
+        option: "Post image where it shows in the post content area",
+        isCorrect: false,
+      },
+      {
+        option: "Post image where it displayed outside of the content area",
+        isCorrect: true,
+      },
+      {
+        option: "Page Image",
+        isCorrect: false,
+      },
+      {
+        option: "Post & Page Image",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText: "How many post category we can add in our website?",
+    options: [
+      {
+        option: "15",
+        isCorrect: false,
+      },
+      {
+        option: "5",
+        isCorrect: false,
+      },
+      {
+        option: "19179826761",
+        isCorrect: false,
+      },
+      {
+        option: "Unlimited",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "Which user roles doesn't come with first wordpress installation??",
+    options: [
+      {
+        option: "Super Admin",
+        isCorrect: true,
+      },
+      {
+        option: "Editor",
+        isCorrect: false,
+      },
+      {
+        option: "Author",
+        isCorrect: false,
+      },
+      {
+        option: "Pilot",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "Which user roles doesn't come with first wordpress installation??",
+    options: [
+      {
+        option: "Super Admin",
+        isCorrect: true,
+      },
+      {
+        option: "Editor",
+        isCorrect: false,
+      },
+      {
+        option: "Author",
+        isCorrect: false,
+      },
+      {
+        option: "Pilot",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText: "Plugin stands for-",
+    options: [
+      {
+        option: "To make Pizza in Pizzaburg",
+        isCorrect: false,
+      },
+      {
+        option: "Adding new features to website",
+        isCorrect: true,
+      },
+      {
+        option: "Jibone nam e shuni nai. Eida diya ki kore?? ",
+        isCorrect: false,
+      },
+      {
+        option: "Install pipe to build a water pump.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText: "Where we can put custom CSS in free theme?",
+    options: [
+      {
+        option: "Appearance > Header",
+        isCorrect: false,
+      },
+      {
+        option: "Settings > General",
+        isCorrect: false,
+      },
+      {
+        option: "Amader barir pison side er building er 3 tolay.",
+        isCorrect: false,
+      },
+      {
+        option:
+          "Mirpur 2 Stadium e giye Dashboard > Appearance > Additional CSS",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  {
+    questionText: "What is Elementor?",
+    options: [
+      {
+        option: "A bus company I've won",
+        isCorrect: false,
+      },
+      {
+        option: "Hobe kisu akta. Agami bosor bolte parbo.",
+        isCorrect: false,
+      },
+      {
+        option: "Eita diya wordpress site design kora jay.",
+        isCorrect: true,
+      },
+      {
+        option: "A desktop software.",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "What is protected post?",
+    options: [
+      {
+        option: "Password Protected Post Type",
+        isCorrect: true,
+      },
+      {
+        option: "Ei post police a pahara dey.",
+        isCorrect: false,
+      },
+      {
+        option: "President e jei post kore oida hoilo giya protected post",
+        isCorrect: false,
+      },
+      {
+        option: "Janina",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText: "Can we set a time and date for publishing post?",
+    options: [
+      {
+        option: "!Yes",
+        isCorrect: false,
+      },
+      {
+        option: "No",
+        isCorrect: false,
+      },
+      {
+        option: "!No",
+        isCorrect: false,
+      },
+      {
+        option: "Yes",
+        isCorrect: true,
+      },
+    ],
+  },
+];
